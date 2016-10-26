7e4373b6ebb34961de8a50b7549121a4976b4012
Tieme Alberts <notify@tieme-alberts.nl>
2016-10-26 18:34:24 +0200

    Disabled the page loader for testing

--- dashboard/elements/header.php
+++ dashboard/elements/header.php
@@ -2,52 +2,52 @@
 <html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <title>Certificate Manager</title>
+	<meta charset="UTF-8">
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+	<title>Certificate Manager</title>
 
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
 
-    <!-- Bootstrap Core Css -->
-    <link href="../assets/plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
+	<!-- Bootstrap Core Css -->
+	<link href="../assets/plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
 
-    <!-- Plugin Css -->
-    <link href="../assets/plugins/node-waves/waves.css" rel="stylesheet" />
-    <link href="../assets/plugins/animate-css/animate.css" rel="stylesheet" />
-    <link href="../assets/plugins/material-design-preloader/md-preloader.css" rel="stylesheet" />
-    <link href="../assets/plugins/multi-select/css/multi-select.css" rel="stylesheet" />
+	<!-- Plugin Css -->
+	<link href="../assets/plugins/node-waves/waves.css" rel="stylesheet" />
+	<link href="../assets/plugins/animate-css/animate.css" rel="stylesheet" />
+	<link href="../assets/plugins/material-design-preloader/md-preloader.css" rel="stylesheet" />
+	<link href="../assets/plugins/multi-select/css/multi-select.css" rel="stylesheet" />
 
-    <!-- Custom Css -->
-    <link href="../assets/css/style.css" rel="stylesheet">
-    <link href="../assets/css/themes/theme-light-blue.min.css" rel="stylesheet" />
+	<!-- Custom Css -->
+	<link href="../assets/css/style.css" rel="stylesheet">
+	<link href="../assets/css/themes/theme-light-blue.min.css" rel="stylesheet" />
 </head>
 
 <body class="theme-light-blue">
-    <!-- Page Loader 
-    <div class="page-loader-wrapper">
-        <div class="loader">
-            <div class="md-preloader pl-size-md">
-                <svg viewbox="0 0 75 75">
-                    <circle cx="37.5" cy="37.5" r="33.5" class="pl-red" stroke-width="4" />
-                </svg>
-            </div>
-            <p>Please wait...</p>
-        </div>
-    </div>
+	<!-- Page Loader 
+	<div class="page-loader-wrapper">
+		<div class="loader">
+			<div class="md-preloader pl-size-md">
+				<svg viewbox="0 0 75 75">
+					<circle cx="37.5" cy="37.5" r="33.5" class="pl-red" stroke-width="4" />
+				</svg>
+			</div>
+			<p>Please wait...</p>
+		</div>
+	</div>
 	-->
-    <!-- #END# Page Loader -->
-    <!-- Overlay For Sidebars -->
-    <div class="overlay"></div>
-    <!-- #END# Overlay For Sidebars -->
-    <!-- Top Bar -->
-    <nav class="navbar">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a href="javascript:void(0);" class="bars"></a>
-                <a class="navbar-brand" href="/">Certificate Manager</a>
-            </div>
-        </div>
-    </nav>
-    <!-- #Top Bar -->
+	<!-- #END# Page Loader -->
+	<!-- Overlay For Sidebars -->
+	<div class="overlay"></div>
+	<!-- #END# Overlay For Sidebars -->
+	<!-- Top Bar -->
+	<nav class="navbar">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a href="javascript:void(0);" class="bars"></a>
+				<a class="navbar-brand" href="/">Certificate Manager</a>
+			</div>
+		</div>
+	</nav>
+	<!-- #Top Bar -->
