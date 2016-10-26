da5cd2d34b38e905aac2496db783007a12db3b7f
Tieme Alberts <notify@tieme-alberts.nl>
2016-10-26 18:29:39 +0200

    minor fix in $page

--- dashboard/elements/sidebar.php
+++ dashboard/elements/sidebar.php
@@ -1,67 +1,67 @@
 <?php
-
 $domain_menu = $func->DA_GET_DOMAINS_MENU();
 
-$page = $_GET['page'];
+$page = '';
+if($_GET){
+	$page = $_GET['page'];	
+}
 ?>
-    <section>
-        <!-- Left Sidebar -->
-        <aside id="leftsidebar" class="sidebar">
-            <!-- Menu -->
-            <div class="menu">
-                <ul class="list">
-                    <li class="header">MAIN NAVIGATION</li>
-                    <li <?php if(!$page){echo 'class="active"';} ?>>
-                        <a href="../">
-                            <i class="material-icons">home</i>
-                            <span>Dashboard</span>
-                        </a>
-                    </li>
-					
-					<li <?php if($page==='domaininfo'){echo 'class="active"';} ?>>
-						<a href="javascript:void(0);" class="menu-toggle waves-effect waves-block">
-							<i class="material-icons">public</i>
-							<span>Domains</span>
-						</a>
-						<ul class="ml-menu" style="display: none;">
-							<?php echo $domain_menu;?>
-						</ul>
-					</li>
-					
-                    <li <?php if($page==='settings'){echo 'class="active"';} ?>>
-                        <a href="?page=settings">
-                            <i class="material-icons">settings</i>
-                            <span>Settings</span>
-                        </a>
-                    </li>					
-                    <li <?php if($page==='password'){echo 'class="active"';} ?>>
-                        <a href="?page=password	">
-                            <i class="material-icons">vpn_key</i>
-                            <span>Change Password</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="../?do=logout">
-                            <i class="material-icons">input</i>
-                            <span>Sign Out</span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <!-- #Menu -->
-            <!-- Footer -->
-            <div class="legal">
-                <div class="copyright">
-                    &copy; 2016 <a target="_blank" href="https://www.tieme-alberts.nl/">Tieme Alberts</a>.
-                </div>
-                <div class="copyright">
-                    <small><a target="_BLANK" href="https://github.com/t-me/DireactAdmin-Certificate-Manager">GitHub Project Page.</a></small>
-                </div>
-                <div class="version">
-                    <b>Version:</b>0.1 Alpha
-                </div>
-            </div>
-            <!-- #Footer -->
-        </aside>
-        <!-- #END# Left Sidebar -->
-    </section>
+<section>
+	<!-- Left Sidebar -->
+	<aside id="leftsidebar" class="sidebar">
+		<!-- Menu -->
+		<div class="menu">
+			<ul class="list">
+				<li class="header">MAIN NAVIGATION</li>
+				<li <?php if(!$page){echo 'class="active"';} ?>>
+					<a href="../">
+						<i class="material-icons">home</i>
+						<span>Dashboard</span>
+					</a>
+				</li>
+				<li <?php if($page==='domaininfo'){echo 'class="active"';} ?>>
+					<a href="javascript:void(0);" class="menu-toggle waves-effect waves-block">
+						<i class="material-icons">public</i>
+						<span>Domains</span>
+					</a>
+					<ul class="ml-menu" style="display: none;">
+						<?php echo $domain_menu;?>
+					</ul>
+				</li>
+				<li <?php if($page==='settings'){echo 'class="active"';} ?>>
+					<a href="?page=settings">
+						<i class="material-icons">settings</i>
+						<span>Settings</span>
+					</a>
+				</li>					
+				<li <?php if($page==='password'){echo 'class="active"';} ?>>
+					<a href="?page=password	">
+						<i class="material-icons">vpn_key</i>
+						<span>Change Password</span>
+					</a>
+				</li>
+				<li>
+					<a href="../?do=logout">
+						<i class="material-icons">input</i>
+						<span>Sign Out</span>
+					</a>
+				</li>
+			</ul>
+		</div>
+		<!-- #Menu -->
+		<!-- Footer -->
+		<div class="legal">
+			<div class="copyright">
+				&copy; 2016 <a target="_blank" href="https://www.tieme-alberts.nl/">Tieme Alberts</a>.
+			</div>
+			<div class="copyright">
+				<small><a target="_BLANK" href="https://bitbucket.org/t-me/direactadmin-certificate-manager">Bitbucket Project Page.</a></small>
+			</div>
+			<div class="version">
+				<b>Version: </b> 0.1
+			</div>
+		</div>
+		<!-- #Footer -->
+	</aside>
+	<!-- #END# Left Sidebar -->
+</section>
