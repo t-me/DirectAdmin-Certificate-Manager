0332e9e72ad79bfd2b90e8fe3dfc98a18f642a9a
Tieme Alberts <notify@tieme-alberts.nl>
2016-10-26 18:36:48 +0200

    Update index.php

--- dashboard/index.php
+++ dashboard/index.php
@@ -12,41 +12,33 @@ $func = new cm_function();
 require_once '../lib/config.php';
 $config = new Config_Lite('../config/config.ini');
 
-
 include 'elements/header.php';
-
 include 'elements/sidebar.php';
-
-
 ?>
 
-    <section class="content">
-        <div class="container-fluid">
-
-				<?php
-				$page = '';
-				if ($_GET){
-					$page = $_GET['page'];
-
-					if($page == 'password') {
-						include 'pages/password.php';
-					} elseif($page == 'domaininfo') {
-						include 'pages/domaininfo.php';
-					}elseif($page == 'settings') {
-						include 'pages/settings.php';
-					} elseif($page == 'encrypt') {
-						include 'pages/encrypt.php';
-					}else{
-						include 'pages/dashboard.php';
-					}
-					
+	<section class="content">
+		<div class="container-fluid">
+		<?php
+			$page = '';
+			if ($_GET){
+				$page = $_GET['page'];
+				if($page == 'password'){
+					include 'pages/password.php';
+				}elseif($page == 'domaininfo'){
+					include 'pages/domaininfo.php';
+				}elseif($page == 'settings'){
+					include 'pages/settings.php';
+				}elseif($page == 'encrypt'){
+					include 'pages/encrypt.php';
 				}else{
 					include 'pages/dashboard.php';
 				}
-				?>
-
-        </div>
-    </section>
+			}else{
+				include 'pages/dashboard.php';
+			}
+		?>
+		</div>
+	</section>
 
 <?php
 include 'elements/footer.php';
